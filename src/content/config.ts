74777de3a6a22cd254505d2118636ef3b05edfe7
andidumitru96 <85399946+andidumitru96@users.noreply.github.com>
2026-02-23 21:01:06 +0200

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,9 @@ const projects = defineCollection({
   schema: z.object({
     title: z.string(),
     year: z.string(),
+    date: z.string(),
     location: z.string(),
+    featured: z.boolean().optional(),
     images: z.array(z.string()),
   }),
 });
