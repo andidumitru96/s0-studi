d7aed28352c57400fd557c9500ca9e95dc4a8264
andidumitru96 <85399946+andidumitru96@users.noreply.github.com>
2026-02-23 23:20:33 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,12 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://s0studio.com",
   output: "static",
+
+});
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://s0studio.com",
+  output: "static",
+  publicDir: "public",
 });
